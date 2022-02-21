c74cd70579235667d73d985f18948c69488bb14a
ColdMeekly <coldmeekly@gmail.com>
2022-02-21 22:24:53 +0000

    Initial Upload

---
+++ include/sdk.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+// Disable warnings for steam_api
+#pragma warning(disable:4996)
+
+#define LUA_BUILD_AS_DLL
+#include "Lua/lua.hpp"
+#include "Steam/steam_api.h"
+#include "MyGUI/MyGUI.h"
+#include "SM/SM.h"
+
+#pragma comment(lib, "lib/lua51.lib")
+#pragma comment(lib, "lib/MyGUIEngine.lib")
